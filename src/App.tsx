fdd818c01ad6efe6f83b171b68875809a2a9e03d
Helton Quintãns <helton.quit@gmail.com>
2022-03-11 14:50:47 -0300

    creation of component/Header folders and index.tsx and styles.ts files for the creation of the Header component that contains a logo and a [New transaction] button, the Header contains 2 containers (Container and content) the styling of both was finished.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { GlobalStyle } from "./styles/global";
+import { Header } from "./components/Header"
+import { GlobalStyle } from "./styles/global"
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello word</h1>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
 
