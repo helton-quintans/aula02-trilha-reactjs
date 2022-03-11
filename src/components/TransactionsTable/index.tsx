8e7409f4d5ccfb684ae67045f2cba01f75797f1b
Helton Quintãns <helton.quit@gmail.com>
2022-03-11 18:28:24 -0300

    Create TransactionsTable component + your styles.ts

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,35 @@
+import { Container } from "./styles";
+
+export function TransactionsTable(){
+    return(
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Value</th>
+                        <th>Category</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de website</td>
+                        <td className="deposit">R$12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>11/03/2022</td>
+                    </tr>
+
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="withdraw">-R$2.000</td>
+                        <td>Moradia</td>
+                        <td>10/03/2022</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+        
+    )
+}
